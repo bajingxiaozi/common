3f2fa64ce61b37d7ec17f3a2512ca8ffaeb8f3ac
youfangxi <youfangxi@ecarx.com.cn>
2018-06-06 17:56:27 +0800

    提交代码

--- src/main/java/com/xyf/common/util/FileUtils2.java
+++ src/main/java/com/xyf/common/util/FileUtils2.java
@@ -1,8 +1,15 @@
 package com.xyf.common.util;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOCase;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 public class FileUtils2 {
@@ -29,6 +36,11 @@ public class FileUtils2 {
         } else {
             Lg.e(TAG, "not a file", file);
         }
+    }
+
+    @Nonnull
+    public static List<File> listImages(@Nonnull File directory) {
+        return Collections.unmodifiableList(new ArrayList<>(FileUtils.listFiles(directory, new SuffixFileFilter(ImageUtils.IMAGE_SUFFIX, IOCase.INSENSITIVE), null)));
     }
 
     @Nonnull
