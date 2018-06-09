b086e4d652a2380988fec530ffde487bc5b2e406
getting.xi <getting.xi@gmail.com>
2018-06-09 18:21:45 +0800

    提交代码

--- src/main/java/com/xyf/common/util/FileUtils2.java
+++ src/main/java/com/xyf/common/util/FileUtils2.java
@@ -2,13 +2,14 @@ package com.xyf.common.util;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOCase;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.io.filefilter.SuffixFileFilter;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.File;
+import java.io.FileFilter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -41,6 +42,11 @@ public class FileUtils2 {
     @Nonnull
     public static List<File> listImages(@Nonnull File directory) {
         return new ArrayList<>(FileUtils.listFiles(directory, new SuffixFileFilter(ImageUtils.IMAGE_SUFFIX, IOCase.INSENSITIVE), null));
+    }
+
+    @Nonnull
+    public static List<File> listDirectories(@Nonnull File directory) {
+        return CollectionUtils.asList(directory.listFiles((FileFilter) DirectoryFileFilter.INSTANCE));
     }
 
     @Nonnull
